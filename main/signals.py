e52dc5d106fa3b7cfd88e420ebb5ad345cdbeb4e
ARYAN <aryanjainak@gmail.com>
2024-02-18 12:41:54 -0800

    push

--- main/signals.py
+++ main/signals.py
@@ -5,10 +5,9 @@ from .models import Player,Game
 @receiver(post_save, sender=Player)
 def create_game_after_3_minutes(sender, instance, created, **kwargs):
     print('ahh')
-    if created:
-        if instance.game.time_difference >= 180:  # 180 seconds = 3 minutes
+    if instance.game.time_difference >= 180:  # 180 seconds = 3 minutes
             # Create a new Game object here if condition is met
-            new_game = Game.objects.create(name=instance.game.name)
+        new_game = Game.objects.create(name=instance.game.name)
             #print('working h ',new_game)
             # You may set other fields of the new_game object here as needed
-            new_game.save()
+        new_game.save()
